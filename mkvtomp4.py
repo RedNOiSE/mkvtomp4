582b62956516e739afd362fc3fc02d4f2f9803a9
RedNOiSE <andrey-ternovskij@yandex.ru>
2017-08-29 17:00:53 +1000

    Initial editing

--- mkvtomp4.py
+++ mkvtomp4.py
@@ -1,1 +1,8 @@
+import os
+import subprocess
+import urllib
+import tmdbsimple as tmdb
+from imdbpie import Imdb
+from mutagen.mp4 import MP4, MP4Cover
 
+tmdb.API_KEY="3720151b81d502aa54a256233f6f5af8"
